7abbfcdb87b558b2ae880dfe51d161f696b52496
Ryan Wright <rwright3702@sdsu.edu>
2021-02-02 15:27:23 -0800

    Generated Mesh and primitive game object.

--- Assets/PGC_Mesh.cs
+++ Assets/PGC_Mesh.cs
@@ -4,10 +4,39 @@ using UnityEngine;
 
 public class PGC_Mesh : MonoBehaviour
 {
+    public int xSize = 20, zSize = 20;
+    private Mesh mesh;
+    private MeshFilter meshFilter;
+    private MeshRenderer meshRenderer;
+    private MeshCollider meshCollider;
+
+    public Vector3[] newVertices;
+    public Vector2[] newUV;
+    public int[] newTriangles;
+
+    private void Awake()
+    {
+        meshFilter = gameObject.AddComponent<MeshFilter>();
+        meshRenderer = gameObject.AddComponent<MeshRenderer>();
+        meshRenderer.material = Resources.Load<Material>("MeshMaterial");
+        meshCollider = gameObject.AddComponent<MeshCollider>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+        mesh.vertices = newVertices;
+        mesh.triangles = newTriangles;
+        //GetComponent<MeshCollider>().convex = true;
         
+        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cube.AddComponent<Rigidbody>();
+        cube.transform.position = new Vector3(10f, 20f, 5f);
+        CreateShape();
+        UpdateMesh();
+        GetComponent<MeshCollider>().sharedMesh = mesh;
     }
 
     // Update is called once per frame
@@ -15,4 +44,62 @@ public class PGC_Mesh : MonoBehaviour
     {
         
     }
+
+    void CreateShape()
+    {
+        newVertices = new Vector3[(xSize + 1) * (zSize + 1)];
+
+        
+        for(int i = 0, z = 0; z <= zSize; z++)
+        {
+            for(int x = 0; x <= xSize; x++)
+            {
+                float y = Mathf.PerlinNoise(x * .3f, z * .3f) * 2f;
+                newVertices[i] = new Vector3(x, y, z);
+                i++;
+            }
+        }
+
+        newTriangles = new int[xSize * zSize * 6];
+
+        int vert = 0;
+        int tris = 0;
+
+        for(int z = 0; z < zSize; z++)
+        {
+            for (int x = 0; x < xSize; x++)
+            {
+                newTriangles[tris + 0] = vert + 0;
+                newTriangles[tris + 1] = vert + xSize + 1;
+                newTriangles[tris + 2] = vert + 1;
+                newTriangles[tris + 3] = vert + 1;
+                newTriangles[tris + 4] = vert + xSize + 1;
+                newTriangles[tris + 5] = vert + xSize + 2;
+
+                vert++;
+                tris += 6;
+
+                
+            }
+            vert++;
+        }
+        
+
+        
+
+    }
+
+    void UpdateMesh()
+    {
+        mesh.Clear();
+
+        mesh.vertices = newVertices;
+        mesh.triangles = newTriangles;
+
+        mesh.RecalculateNormals();
+    }
+
+    
+
+    
 }
